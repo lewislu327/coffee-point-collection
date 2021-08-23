4ffb793e919e07c90716414d010a3cd5e79aa1e5
lewislu327 <agooday32@gmail.com>
2021-08-23 22:58:09 +0800

    feat: add facebook login feature

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const authRoute = require('./modules/auth')
 const homeRoute = require('./modules/home')
 const userRoute = require('./modules/user')
 const adminRoute = require('./modules/admin')
@@ -8,6 +9,7 @@ const { authenticator, authenticatedAdmin } = require('../middlewares/auth')
 
 router.use('/admin', authenticatedAdmin, adminRoute)
 router.use('/users', userRoute)
+router.use('/auth', authRoute)
 router.use('/', authenticator, homeRoute)
 
 module.exports = router
